11b529946e19ccad90b2661400e856f00eca9d71
Duc Chu <ducchu@microsoft.com>
2021-12-18 23:21:55 -0600

    Deplay data on homepage

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Task> Tasks { get; set; }
+
+        private ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Tasks = _context.Tasks
+                .OrderBy(x => x.Priority);
         }
     }
 }
